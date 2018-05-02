bbd2dd9f0aab04572afe201b722bd620a2a23793
YuanPeikai <2908363622@qq.com>
2018-05-02 10:44:08 +0800

    添加了阿里巴巴的路由框架

--- appTourism/src/main/java/com/example/componentizationdaemon/MainActivity.java
+++ appTourism/src/main/java/com/example/componentizationdaemon/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.componentizationdaemon;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.example.mylibrary.utils.DensityUtils;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
 
         int i = DensityUtils.dp2px(this, 20f);
         Log.i(TAG, "onCreate: i=" + i);
+
+        findViewById(R.id.manor).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick: ddd");
+                ARouter.getInstance().build("/manor_/MainActivity").navigation();
+
+
+            }
+        });
 
 
     }
--- manor_module/src/main/java/com/example/manorapp/MainActivity.java
+++ manor_module/src/main/java/com/example/manorapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.manorapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.alibaba.android.arouter.facade.annotation.Route;
+
+@Route(path = "/manor_/MainActivity")
 public class MainActivity extends AppCompatActivity {
 
     @Override
